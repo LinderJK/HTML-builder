58a20d7074eb2d31214d59fef7cfc6d5deec5918
LinderJK <slepchenkovia@gmail.com>
2024-01-21 17:42:04 +0300

    fix: 01-read-file absolute path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const basePath = '../HTML-builder/01-read-file';
 const fineName = 'text.txt';
-const absolutePath = path.resolve(basePath, fineName);
-
+const absolutePath = path.resolve(__dirname, fineName);
 const stream = fs.createReadStream(absolutePath);
 let result = '';
+
 stream.on('data', (chunk) => {
   result += chunk;
 });
