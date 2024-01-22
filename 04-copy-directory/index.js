bccd8f61cff8e48bee70191e5fd20a6929b081b7
LinderJK <slepchenkovia@gmail.com>
2024-01-22 19:25:10 +0300

    feat: add create dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,3 +3,14 @@ const path = require('path');
 
 const dirName = 'files';
 const dir = path.resolve(__dirname, dirName);
+
+async function copyDir() {
+  const newDirPath = path.join(__dirname, 'files-copy');
+
+  await fs.mkdir(newDirPath, { recursive: true });
+  const files = await fs.readdir(dir);
+
+
+}
+
+copyDir();
