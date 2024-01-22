61d4f976ce29d3335f8c3de801f6e434e8f51b29
LinderJK <slepchenkovia@gmail.com>
2024-01-22 20:01:55 +0300

    feat: 06 add recursive function copy dir assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const distrDirName = 'project-dist';
 const distrDir = path.resolve(__dirname, distrDirName);
-
+const assetsPath = path.join(__dirname, 'assets');
+const assetsCopyPath = path.join(distrDir, 'assets');
 
 const createDir = async () => {
   await fs.mkdir(distrDir, { recursive: true});
@@ -38,12 +39,53 @@ async function replaceTags() {
   await fs.writeFile(outputFile, resultContent, 'utf-8');
 }
 
+async function buildCss () {
+  const cssPath = path.join(__dirname, 'styles');
+  const cssBuildFile = path.join(distrDir, 'style.css');
 
+  const files = await fs.readdir(cssPath);
 
+  const cssFiles = files.filter((file) => path.extname(file) === '.css');
+  console.log(cssFiles, 'cssFiles');
+
+  const fileContentsPromises = cssFiles.map(async (cssFile) => {
+    const filePath = path.join(cssPath, cssFile);
+    return await fs.readFile(filePath, 'utf-8');
+  });
+
+  const fileContents = await Promise.all(fileContentsPromises);
+  await fs.writeFile(cssBuildFile, fileContents.join('\n'), 'utf-8');
+}
+
+async function copyDir(source, destination) {
+  const stats = await fs.stat(source);
+  if (stats.isFile()) {
+    await fs.copyFile(source, destination);
+  } else if (stats.isDirectory()) {
+    await copyDirRecursive(source, destination);
+  }
+}
+
+async function copyDirRecursive(source, destination) {
+  await fs.mkdir(destination, { recursive: true });
+  const files = await fs.readdir(source);
+
+  for (const file of files) {
+    const sourcePath = path.join(source, file);
+    const destinationPath = path.join(destination, file);
+
+    await copyDir(sourcePath, destinationPath);
+  }
+}
 
 (async () => {
   try {
-    await Promise.all([createDir(), replaceTags()]);
+    await Promise.all([
+      createDir(),
+      replaceTags(),
+      buildCss(),
+      copyDir(assetsPath, assetsCopyPath),
+    ]);
     console.log('Build completed successfully.');
   } catch (error) {
     console.error(`Error: ${error.message}`);
