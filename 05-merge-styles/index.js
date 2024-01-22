7200cd63494e76977b823afb2a058e5d850d94ae
LinderJK <slepchenkovia@gmail.com>
2024-01-22 19:33:17 +0300

    feat: 05 read dir styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,12 +1,16 @@
 const fs = require('fs/promises');
 const path = require('path');
-
+const { error } = require('console');
+const { stat } = require('fs');
 
 const stylesDir = path.resolve(__dirname, 'styles');
 const distDir = path.resolve(__dirname, 'project-dist');
 const cssBundle = path.join(distDir, 'bundle.css');
 
 async function cssMerge() {
+  const files = await fs.readdir(stylesDir);
+  console.log(files, 'files');
+
 
 }
 
