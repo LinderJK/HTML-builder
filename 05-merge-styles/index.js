5e49d506a0ebd803d6ef7ff13d9d1dd5d6074ef3
LinderJK <slepchenkovia@gmail.com>
2024-01-22 19:32:40 +0300

    feat: 05 add dirs and modules

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+
+const stylesDir = path.resolve(__dirname, 'styles');
+const distDir = path.resolve(__dirname, 'project-dist');
+const cssBundle = path.join(distDir, 'bundle.css');
+
+async function cssMerge() {
+
+}
+
+cssMerge();
